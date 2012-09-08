7168f268973d5e69cb72fa042ceec420e67eebf1
Bruce Bjorklund <bbjorklund@gaspowered.com>
2012-09-08 16:38:57 -0700

    Intial Commit
    Everything for an initial commit

---
+++ app.js
@@ -0,0 +1,7 @@
+/**
+ *
+ * Project: Legends of the Jedi Character-Website-Thingy
+ * Created by: Veska AKA Bruce Bjorklund
+ * Date: 9/6/12
+ * insert copyright stuff here
+ */
---
+++ assets/javascript/script.js
@@ -0,0 +1,10 @@
+/* Author:
+
+*/
+
+
+
+$('a[data-toggle="tab"]').on('show', function (e) {
+e.target // activated tab
+e.relatedTarget // previous tab
+});
