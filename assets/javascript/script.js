0d24599555bac447a27187d857537406763e656a
Bruce Bjorklund <bbjorklund@gaspowered.com>
2012-09-08 18:33:15 -0700

    integrated socket.io

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -8,3 +8,35 @@ $('a[data-toggle="tab"]').on('show', function (e) {
 e.target // activated tab
 e.relatedTarget // previous tab
 });
+
+var socket = socket = io.connect('localhost');
+////////////////////////
+// Socket.IO Logic
+function socketConfigure()
+	{
+	socket.on('connect', function ()
+		{
+		debugger;
+		});
+	socket.on('reconnect', function ()
+		{
+
+		});
+	socket.on('reconnecting', function (nextRetry)
+		{
+
+		});
+	socket.on('reconnect_failed', function ()
+		{
+
+		});
+	registerMessage("Login");
+	}
+
+function registerMessage(message)
+	{
+	socket.emit('register', message);
+	}
+
+socketConfigure();
+
